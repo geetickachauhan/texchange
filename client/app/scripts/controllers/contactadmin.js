9517fe3c6bfd5e7745743af180f989b5ef628329
geetickachauhan <gchau005@fiu.edu>
2016-11-11 16:10:30 -0500

    worked some more on contact admin-figuring out file stuff

--- client/app/scripts/controllers/contactadmin.js
+++ client/app/scripts/controllers/contactadmin.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name clientApp.controller:ContactadminCtrl
@@ -9,12 +8,28 @@
  */
 angular.module('clientApp')
   .controller('ContactadminCtrl', function ($scope) {
-    $scope.contactAdmin = { }
+    $scope.contactAdminObj = { }
 
-    console.log($scope.contactAdmin)
+    console.log($scope.contactAdmin);
 
-    $scope.contactAdmin = function(){
+    $scope.contactAdminFn = function(){
+      window.alert("Thank you. Your Message has been sent successfully to the Admin.");
 
-
+      var file = new File([""], "contactAdmin.txt", {type: "text/plain;charset=utf-8"});
+      //file.open("contactAdmin.txt");
+      /*
+      var fs = require("contactAdmin");
+      var file = fs.readFileSync("contactAdmin.txt", "UTF-8");
+      console.log(file);
+      */
+      //var blob = new Blob([""], {type: "text/plain;charset=utf-8"});
+      /*
+      var file = new File([""], "contactAdmin.txt", {type: "text/plain;charset=utf-8"})
+      file.open("w"); // open file with write access
+      file.writeln($scope.contactAdminObj.type);
+      file.writeln($scope.contactAdminObj.subject);
+      file.writeln($scope.contactAdminObj.message);
+      file.close();
+      */
     }
   });
