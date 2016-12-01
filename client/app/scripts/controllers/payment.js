c210fee1fb480f9c52824aa98c61bf7c949b25f0
geetickachauhan <gchau005@fiu.edu>
2016-11-30 22:22:22 -0500

    fixed up payment.html with novalidate and the js file

--- client/app/scripts/controllers/payment.js
+++ client/app/scripts/controllers/payment.js
@@ -8,57 +8,52 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-.controller('PaymentCtrl', function ($scope, $rootScope, $location, Book, User) 
+.controller('PaymentCtrl', function ($scope, $rootScope, $location, Book, User)
 {
 	$scope.cart = $rootScope.user.cart;
 	console.log($scope.cart);
-	
+
 
 	// Pay books in DB
 	$scope.pay = function()
 	{
 		console.log('payment', $scope.paymentInfo);
 
-		var ifError = false;
+
 
 			if($scope.paymentInfo.shipping_address.length > 40)
             {
               ifError = true;
-              $scope.showErrorMessage("Shipping address is too long", "Ok", function(){
-                return;
-              });
+              $scope.showErrorMessage("Shipping address is too long", "OK", function(){ });
+							return;
             }
 
             if($scope.paymentInfo.billing_address.length > 40)
             {
               ifError = true;
-              $scope.showErrorMessage("Billing address is too long", "Ok", function(){
-                return;
-              });
+              $scope.showErrorMessage("Billing address is too long", "OK", function(){ });
+							return;
             }
 
             if($scope.paymentInfo.cardholder_name.length > 40)
             {
               ifError = true;
-              $scope.showErrorMessage("Cardholder's name is too long", "Ok", function(){
-                return;
-              });
+              $scope.showErrorMessage("Cardholder's name is too long", "OK", function(){ });
+							return;
             }
 
             if($scope.paymentInfo.card_number.length !== 16)
             {
               ifError = true;
-              $scope.showErrorMessage("Card number must be only 16 digits long", "Ok", function(){
-                return;
-              });
+              $scope.showErrorMessage("Card number must be only 16 digits long", "OK", function(){});
+							return;
             }
 
             if(isNaN($scope.paymentInfo.card_number))
             {
             	ifError = true;
-            	$scope.showErrorMessage("Card number field must only be numerical input", "Ok", function(){
-                return;
-              });
+            	$scope.showErrorMessage("Card number field must only be numerical input", "OK", function(){ });
+							return;
             }
 
 
@@ -70,54 +65,43 @@ angular.module('clientApp')
            	  		if(parseInt($scope.paymentInfo.card_exp.substr(0, 2)) > 12 || parseInt($scope.paymentInfo.card_exp.substr(0, 2)) < 1)
            	  		{
            	  			ifError = true;
-           	  			$scope.showErrorMessage("Expiration date month must be between 01 and 12", "Ok", function(){
-                			return;
-              			});
+           	  			$scope.showErrorMessage("Expiration date month must be between 01 and 12", "OK", function(){});
+										return;
            	  		}
 
            	  		if(parseInt($scope.paymentInfo.card_exp.substr(3, 2)) > 31 || parseInt($scope.paymentInfo.card_exp.substr(3, 2)) < 1)
            	  		{
            	  			ifError = true;
-           	  			$scope.showErrorMessage("Expiration date day must be between 01 and 31", "Ok", function(){
-                			return;
-              			});
+           	  			$scope.showErrorMessage("Expiration date day must be between 01 and 31", "OK", function(){ });
+										return;
            	  		}
            	  	}
            	  	else
            	  	{
            		  	ifError = true;
-           		  	$scope.showErrorMessage("Expiration date be in the correct format (MM/DD)1", "Ok", function(){
-            	    	return;
-            	  	});
+           		  	$scope.showErrorMessage("Expiration date be in the correct format (MM/DD)1", "OK", function(){ });
+									return;
            		}
            	}
            	else
            	{
            		ifError = true;
-           		$scope.showErrorMessage("Expiration date be in the correct format (MM/DD)", "Ok", function(){
-                	return;
-              	});
+           		$scope.showErrorMessage("Expiration date be in the correct format (MM/DD)", "OK", function(){});
+							return;
            	}
 
             if($scope.paymentInfo.card_cvv.length !== 3)
             {
               ifError = true;
-              $scope.showErrorMessage("CVV code must be only 3 digits long", "Ok", function(){
-                return;
-              });
+              $scope.showErrorMessage("CVV code must be only 3 digits long", "OK", function(){ });
+							return;
             }
 
             if(isNaN($scope.paymentInfo.card_cvv))
             {
             	ifError = true;
-            	$scope.showErrorMessage("CVV field must only be numerical input", "Ok", function(){
-                return;
-              });
-            }
-
-            if (ifError === true)
-            {
-              return;
+            	$scope.showErrorMessage("CVV field must only be numerical input", "OK", function(){ });
+							return;
             }
 
 		for(var i = 0; i < $scope.cart.length; i++)
@@ -128,7 +112,7 @@ angular.module('clientApp')
   		$scope.cart.splice(0, $scope.cart.length);
   		User.updateCart($rootScope.user._id, $scope.cart).then(function(res){
   			$rootScope.user.cart = res.data.cart;
-  			$location.path('/'); 
+  			$location.path('/');
 			$scope.showErrorMessage('Thank you for your purchase! We hope you ejoyed using Texchange!', "Ok", function(){
 				return;
 			});
@@ -136,9 +120,9 @@ angular.module('clientApp')
 		Book.emptyCart($rootScope.user._id, $scope.cart[i]).then(function(res){
 			console.log(res.data)
   			$rootScope.user.cart = res.data;
-  			$location.path('/'); 
+  			$location.path('/');
   		})
-  		
+
 	}
 
 });
